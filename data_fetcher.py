b50116ce8db6f6274ff7cd12899f345b70022049
Sazan Haji <sazan.hasanhaji@gmail.com>
2026-03-02 18:24:33 +0000

    Move API key to .env and add gitignore

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,20 +1,14 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-API_KEY = "1T0hvkJJmSVQb7dw5XNJqdeI19zfPGtKdaObEflj"
+load_dotenv()  # loads variables from .env
+
+API_KEY = os.getenv("API_KEY")
 API_URL = "https://api.api-ninjas.com/v1/animals"
 
 
 def fetch_data(animal_name):
-    """
-    Fetches the animals data for the animal 'animal_name'.
-    Returns: a list of animals, each animal is a dictionary:
-    {
-      'name': ...,
-      'taxonomy': {...},
-      'locations': [...],
-      'characteristics': {...}
-    }
-    """
     headers = {"X-Api-Key": API_KEY}
     params = {"name": animal_name}
 
